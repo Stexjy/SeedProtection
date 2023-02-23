b90fe23c2f1773ba198bb645ea520c7f6172cbfe
Stefano Parisi <gooste10@gmail.com>
2023-02-23 22:23:35 +0100

    Created Project

---
+++ src/main/java/stexjy/seedprotection/main/SeedProtection.java
@@ -0,0 +1,18 @@
+package stexjy.seedprotection.main;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.PluginBase;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SeedProtection extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
