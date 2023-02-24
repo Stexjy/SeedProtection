b166295cf163fb97fc466e9a902302a143b43853
Stefano Parisi <gooste10@gmail.com>
2023-02-24 17:58:28 +0100

    Added jump breaking prevention

--- src/main/java/stexjy/seedprotection/events/SeedProtectionListener.java
+++ src/main/java/stexjy/seedprotection/events/SeedProtectionListener.java
@@ -2,12 +2,16 @@ package stexjy.seedprotection.events;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
 import org.bukkit.block.data.Ageable;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 
 public class SeedProtectionListener implements Listener {
 
@@ -19,10 +23,25 @@ public class SeedProtectionListener implements Listener {
 
         if(player.isSneaking()) return;
         if(!isSeed(block)) return;
-
         if(canBeBroken(block)) return;
 
         player.sendMessage(ChatColor.RED + "Please shift to break the seed.");
+        event.setCancelled(true);
+    }
+
+    @EventHandler
+    public void onSeedStep(PlayerInteractEvent event) {
+        final Player player = event.getPlayer();
+        final Action action = event.getAction();
+        final Block block = event.getClickedBlock();
+
+        if(action != Action.PHYSICAL) return;
+        if(block.getType() != Material.FARMLAND) return;
+
+        final Block seed = block.getRelative(BlockFace.UP);
+
+        if(canBeBroken(seed)) return;
+
         event.setCancelled(true);
     }
 
